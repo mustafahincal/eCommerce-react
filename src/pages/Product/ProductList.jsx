b5ba74319acfaf17a12eda47546716646a2e0eec
mustafahincal <mustafahncal@gmail.com>
2022-09-20 20:30:04 +0300

    add ProductList page

--- src/pages/Product/ProductList.jsx
+++ src/pages/Product/ProductList.jsx
@@ -1,16 +1,6 @@
-import {
-  Button,
-  Table,
-  TableCaption,
-  TableContainer,
-  Tbody,
-  Td,
-  Tfoot,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
 import { getProducts } from "../../services/productService";
 
 function ProductList() {
@@ -25,72 +15,13 @@ function ProductList() {
 
   return (
     <div>
-      <TableContainer>
-        <Table variant="striped">
-          <Thead>
-            <Tr>
-              <Th>#</Th>
-              <Th>Product Name</Th>
-              <Th>Unit Price</Th>
-              <Th>Units In Stock</Th>
-              <Th>Quantity Per Unit</Th>
-              <Th>Category</Th>
-              <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {products.map((product) => (
-              <Tr key={product.productId}>
-                <Td>{product.productId}</Td>
-                <Td>{product.productName}</Td>
-                <Td>{product.unitPrice.toFixed(2)}</Td>
-                <Td>{product.unitsInStock}</Td>
-                <Td>{product.quantityPerUnit}</Td>
-                <Td>{product.category.categoryName}</Td>
-                <Td>
-                  <Button colorScheme="whatsapp" variant="outline">
-                    Buy
-                  </Button>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+      <Grid templateColumns={"repeat(3,1fr)"} gap="4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Grid>
     </div>
-    // <Table striped bordered hover variant="dark">
-    //   <thead>
-    //     <tr>
-    //       <th>#</th>
-    //       <th>Product Name</th>
-    //       <th>Unit Price</th>
-    //       <th>Units In Stock</th>
-    //       <th>Quantity Per Unit</th>
-    //       <th>Category</th>
-    //       <th></th>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     {products.map((product) => (
-    //       <tr key={product.productId}>
-    //         <td>{product.productId}</td>
-    //         <td>{product.productName}</td>
-    //         <td>{product.unitPrice.toFixed(2)}</td>
-    //         <td>{product.unitsInStock}</td>
-    //         <td>{product.quantityPerUnit}</td>
-    //         <td>{product.category.categoryName}</td>
-    //         <td>
-    //           <Button
-    //             onClick={() => handleBuyProduct(product.productId)}
-    //             variant="primary"
-    //           >
-    //             Buy
-    //           </Button>
-    //         </td>
-    //       </tr>
-    //     ))}
-    //   </tbody>
-    // </Table>
   );
 }
 
